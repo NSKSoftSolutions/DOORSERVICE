bdd780771ecc5bac7c717d250f3c8a7c654b916a
unknown <navabsajjad.ali@gmail.com>
2022-01-22 21:27:08 +0530

    "added a method to get DoorDet"

--- src/main/java/com/nsk/enterprises/controller/DoorController.java
+++ src/main/java/com/nsk/enterprises/controller/DoorController.java
@@ -1,5 +1,23 @@
 package com.nsk.enterprises.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nsk.enterprises.domain.Door;
+import com.nsk.enterprises.dto.DoorDTO;
+
+@RestController
+@RequestMapping(path = "/api")
 public class DoorController {
+
+	@Autowired
+	DoorDTO doorDTO;
+
+	@GetMapping("/doors")
+	public Door getDoorDet() {
+		return new Door(doorDTO.getDoorId(), doorDTO.getDoorSize(), doorDTO.getDoorType());
+	}
 
 }
