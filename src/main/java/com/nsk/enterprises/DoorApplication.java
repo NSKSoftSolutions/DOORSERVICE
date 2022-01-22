34dfe9fd2aa9d185205e82edbb295501d40c7b5a
unknown <navabsajjad.ali@gmail.com>
2022-01-22 21:32:58 +0530

    "added DoorApplication.java"

---
+++ src/main/java/com/nsk/enterprises/DoorApplication.java
@@ -0,0 +1,13 @@
+package com.nsk.enterprises;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoorApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
